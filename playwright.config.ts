fb5501ecfb07ea28129a7de6c673c67bd3763a05
Hrqlv <henriquelopesvelozo@gmail.com>
2024-11-06 18:08:24 -0300

    Ajustes timeout config para yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ dotenv.config();
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 120000,
+  timeout: 180000,
 
   testDir: './tests',
   /* Run tests in files in parallel */
